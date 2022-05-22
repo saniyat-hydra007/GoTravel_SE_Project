5c8ef1bbeebf2a2ccc1758db03aa6f71138b23fd
sooha <fahmad192093@bscse.uiu.ac.bd>
2022-05-22 15:32:47 +0600

    rest is here

---
+++ tapi.php
@@ -0,0 +1,40 @@
+<?php
+// $l_id = $_GET['l_id'];
+  $seat = $_GET['seat'];
+//  $a_seat = $_GET['a_seat'];
+//  $j_date = $_GET['j_date'];
+
+
+// echo $l_id." ".$seat." ".$a_seat." ".$j_date;
+
+
+
+error_reporting(0);
+
+
+$link = mysqli_connect("localhost", "root", "", "go_travel");
+$content ;
+if (mysqli_connect_errno()) {
+    $status = 'ERROR';
+    $content = mysqli_connect_error();
+}
+$query = "UPDATE `train_seat` SET `availability`='y' WHERE id=$seat ";
+$query_run = (mysqli_query($link, $query));
+
+$queryw = "UPDATE `train_seat` SET `userr`='NULL',`pass`='NULL'  WHERE id=$seat";
+$query_run = (mysqli_query($link, $queryw));
+
+
+
+$query1 = "SELECT availability,n,id FROM `train_seat` WHERE id=$seat";
+if ($result = mysqli_query($link, $query1)) {
+    /* fetch associative array */
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+       
+        $content= $row['id']; // push value to array
+    }
+}
+
+header('Content-type: application/json');
+echo json_encode($content); // get all products in jsonformat.
---
+++ tapi3.php
@@ -0,0 +1,35 @@
+<?php
+// $l_id = $_GET['l_id'];
+  $l_id = $_GET['l_id'];
+//  $a_seat = $_GET['a_seat'];
+//  $j_date = $_GET['j_date'];
+
+
+// echo $l_id." ".$seat." ".$a_seat." ".$j_date;
+
+
+
+error_reporting(0);
+
+
+$link = mysqli_connect("localhost", "root", "", "go_travel");
+$content ;
+if (mysqli_connect_errno()) {
+    $status = 'ERROR';
+    $content = mysqli_connect_error();
+}
+$queryw = "UPDATE `train_seat` SET pay='paid' WHERE  `userr`='sooha' and `pass`=123 and l_id=$l_id ";
+$query_run = (mysqli_query($link, $queryw));
+
+$query1 = "SELECT pay FROM `train_seat`  WHERE l_id=$l_id and `userr`='sooha' and pay='paid'";
+if ($result = mysqli_query($link, $query1)) {
+    /* fetch associative array */
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+       
+        $content= $row['pay']; // push value to array
+    }
+}
+
+header('Content-type: application/json');
+echo json_encode($content); // get all products in jsonformat.
